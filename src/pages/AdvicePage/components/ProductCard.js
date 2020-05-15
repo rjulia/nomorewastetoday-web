5471382b191ed4bb54e49d81453815f26b9bdc5f
Ramon Julia <ramon@tofugear.com>
2020-05-15 19:46:29 +0800

    add mobile class in products

--- src/pages/AdvicePage/components/ProductCard.js
+++ src/pages/AdvicePage/components/ProductCard.js
@@ -3,7 +3,7 @@ import { BoxImage, Paragraph, Button } from '../../../components';
 
 const ProductCard = ({ product }) => {
   return (
-    <div className="col-4">
+    <div className="col-12 col-md-4">
       <BoxImage variable={'frame-image yes'} img={product.imageUrl} height={300} />
       <Paragraph variable={'advice__author'} text={product.description__en} tag={'p'} />
       <a href={product.link} target="_blank" rel="noopener noreferrer">
